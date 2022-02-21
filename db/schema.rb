5e25cb7a9ff5075600f1e2b3793fbb3d1350a0dc
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-21 22:34:46 +0545

    feat: generate message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_155221) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_164754) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.boolean "is_private", default: false
@@ -24,4 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_155221) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
