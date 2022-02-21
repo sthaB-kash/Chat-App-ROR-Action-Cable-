a75c0ee6c0c990845fc10b32b3e67741443058da
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-21 22:32:14 +0545

    feat: show respective room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,6 +7,16 @@ class RoomsController < ApplicationController
     @users = User.all_except(@current_user)
   end
 
+  def show
+    @current_user = current_user
+    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room = Room.new
+  
+    render "index"
+  end
+
   def create
     @room = Room.create(name: params["room"]["name"])
   end
