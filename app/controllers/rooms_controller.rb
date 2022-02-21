ab1bea1a725a52c2f3bdae522afcbe3219cd3f79
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-21 22:23:54 +0545

    create room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,12 @@ class RoomsController < ApplicationController
   def index
     @current_user = current_user
     redirect_to '/signin' unless @current_user
+    @room = Room.new 
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
+  end
+
+  def create
+    @room = Room.create(name: params["room"]["name"])
   end
 end
