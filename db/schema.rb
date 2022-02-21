c9bab4b62d36c4a96deb96352ddf426bf5aabf33
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-21 21:40:31 +0545

    feat: create room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_154759) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_155221) do
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
